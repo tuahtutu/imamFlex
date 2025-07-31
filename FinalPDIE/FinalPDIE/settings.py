e5b69a3a578bae9a58f09a0f2ba0c0643ea5b3ad
Aina Irdina <irdinaizzraimy@gmail.com>
2025-07-31 13:33:59 +0800

    Update settings.py

--- FinalPDIE/FinalPDIE/settings.py
+++ FinalPDIE/FinalPDIE/settings.py
@@ -125,8 +125,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'ImamFlex/static'),
 ]
+
 
 
 # Default primary key field type
