28b4f10c9bb61b56a0b4897d46df94d5e094200f
Aina Irdina <irdinaizzraimy@gmail.com>
2025-07-31 12:31:04 +0800

    Update settings.py

--- FinalPDIE/FinalPDIE/settings.py
+++ FinalPDIE/FinalPDIE/settings.py
@@ -117,10 +117,11 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
